88dea593b1f429b8985091aeb7a5a906dfc9bff8
Nectar-Infotel <cgbackoffice@nectarinfotel.in>
2025-06-16 16:02:16 +0530

    Switched from bcryptjs to bcrypt

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcrypt');
 const Admin = require("../models/User"); // ✅ Corrected path (you are using User.js)
 
 // ✅ POST /api/admin/register
